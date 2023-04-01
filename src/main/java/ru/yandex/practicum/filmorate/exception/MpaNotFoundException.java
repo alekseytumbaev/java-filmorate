3b7b255dc0128cffde33c9db1f346783fd84850e
alekseytumbaev <leha.tumbaev@gmail.com>
2023-04-01 15:40:17 +0700

    style: code style

--- src/main/java/ru/yandex/practicum/filmorate/exception/MpaNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/MpaNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class MpaNotFoundException extends EntityNotFoundException{
+public class MpaNotFoundException extends EntityNotFoundException {
     public MpaNotFoundException(String message, long id) {
         super(message, id);
     }
